63551d9bc33edd6e9bc7fcc73d894eaca172f92b
Cole <it@kanamak.com>
2016-01-21 11:39:43 -0600

    removed improperly cloned submodule purchase-workflow

--- addons-extra/purchase-workflow/purchase_order_revision/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import model
-
-
-def fill_unrevisioned_name(cr, registry):
-    cr.execute("UPDATE purchase_order set unrevisioned_name=name "
-               "WHERE unrevisioned_name IS NULL")
